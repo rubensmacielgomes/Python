466186a2795c7b4942cd72eeceeaa52e12e1ea26
rubensmacielgomes <rubensmgomes@gmail.com>
2026-03-09 21:13:08 -0300

    Update Code: 09/03/26-21:13

--- sensor.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    float tensao = 220.5;
-    printf("--- Monitor de Energia RMG ---\n");
-    printf("Status: Ativo\n");
-    printf("Leitura: %.1f Volts\n", tensao);
-    return 0;
-}
