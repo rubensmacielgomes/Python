2caf9b289468cefc48bd670b9dd24b434647d8bc
rubensmacielgomes <rubensmgomes@gmail.com>
2026-03-20 23:02:45 -0300

    Update Code: 20/03/26-23:02

--- src/Projetos/projeto_1.py
+++ src/Projetos/projeto_1.py
@@ -3,3 +3,4 @@ idade = int(input("Digite sua idade: "))
 altura = float(input("Digite sua altura: "))
 print(f"Seu nome é {name}, sua idade é {idade} e sua altura é {altura:.2f}")
 print(f"{name} é idoso") if idade >= 60 else print(f"{name} é jovem")
+nome: int = "Rubens"
